91d0f7e82e23e9cdf62875ca46ec32403648e8b2
Mehak <96226khan@gmail.com>
2022-07-07 14:28:53 +0530

    second change

--- router/studentRoute.js
+++ router/studentRoute.js
@@ -3,10 +3,8 @@ const stuRouter = new express.Router()
 
 const { RegisternewStu, loginStudent, } = require('../controller/studentController.js')
 
-
-
 //get students
-stuRouter.post('/signup',RegisternewStu)
+stuRouter.post('/signup',RegisternewStu)    //first commit
 stuRouter.post('/login', loginStudent)     //testing
 
 
